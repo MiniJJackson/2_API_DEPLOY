f92dbe1106a1c745f41f03c2437df57e7422a040
MiniJJackson <116001889+MiniJJackson@users.noreply.github.com>
2024-10-04 14:19:47 +0200

    feat: messages in const

--- routes/api/v1/messages.js
+++ routes/api/v1/messages.js
@@ -2,16 +2,22 @@
 const express = require('express');
 const router = express.Router();
 
+//DATA messages
+const messages = [
+    { id: 1, user: "Jenna", message: "We got this" },
+    { id: 2, user: "Jackson", message: "Yes we do!" },
+    { id: 3, user: "Pam", message: "We go go!" },
+    { id: 4, user: "Jack", message: "Do we do!" },
+
+];
+
 // GET /api/v1/messages
 router.get('/', (req, res, next) => {
     res.status(200).json({
         status: "success",
         message: 'GET messages!',
         data: {
-            messages: [
-                { id: 1, user: "Jenna", message: "We got this" },
-                { id: 2, user: "Jackson", message: "Yes we do!" }
-            ]
+            messages: messages
         }
     });
 });
