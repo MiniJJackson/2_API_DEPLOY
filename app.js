70c947d3fe8bcf27bc13a29108bf51086e294353
MiniJJackson <116001889+MiniJJackson@users.noreply.github.com>
2024-10-10 23:06:49 +0200

    update: apps.js file updated

--- app.js
+++ app.js
@@ -4,46 +4,47 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-//cors
+// cors
 const cors = require('cors');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// Create an Express application
+var app = express();
 
+// Middleware
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-// cors
-app.use(cors());
 
-// CORS - same-origin policy - block requests from other origins
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
+// View engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-app.use('/api/v1/messages', require('./routes/api/v1/messages'));
+// Routers
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var messagesRouter = require('./routes/api/v1/messages');
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+// Use the routers
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/api/v1/messages', messagesRouter);
+
+// Catch 404 and forward to error handler
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+// Error handler
+app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
 
+// Export the app module
 module.exports = app;
